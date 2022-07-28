52069e40caf81f13d67572c83b92da6929180e68
Simone Di Paolo <s.dipaolo90@gmail.com>
2022-07-28 19:30:30 +0200

    Feature - Calculator Fragment has been created. Numbers added, textfield for total added.

--- app/src/main/java/com/dev/simonedipaolo/cashregister/MainActivity.java
+++ app/src/main/java/com/dev/simonedipaolo/cashregister/MainActivity.java
@@ -1,8 +1,12 @@
 package com.dev.simonedipaolo.cashregister;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.dev.simonedipaolo.cashregister.fragments.CashRegisterFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction()
+                .add(R.id.fragmentPlaceholder, new CashRegisterFragment());
+        fragmentTransaction.commit();
     }
+
 }
